ccf430e595d1525cc9cf4f5c19dd64be62acb95a
bala <bala@ost.com>
2019-08-02 18:21:21 +0530

    Package json file for each resize action to support lambda function

--- compress_and_upload.sh
+++ compress_and_upload.sh
@@ -2,16 +2,31 @@
 
 function run(){
 
+    pkg_id=$1
+
+    if [[ -z ${pkg_id} ]]; then
+        echo "Package file identifier required!"
+        exit 1
+    fi
+
     dir=$(pwd);
-    actualDir=$(realpath $dir)
-    prevDir=`cd $actualDir ; cd ..; pwd`
+    export actualDir=$(realpath $dir)
+    export prevDir=`cd $actualDir ; cd ..; pwd`
 
     echo "actualDir: $actualDir"
     echo "prevDir: $prevDir"
 
     chmod -R 755 $actualDir
 
-    file="${prevDir}/lambda.zip"
+    file="${prevDir}/lambda_${pkg_id}.zip"
+
+    package_file="package_${pkg_id}.json"
+
+    cp -r ${actualDir}/${package_file} ${actualDir}/package.json
+
+    rm -rf ${actualDir}/node_modules ${actualDir}/package-lock.json
+
+    `cd ${actualDir} ; npm install --production`
 
     if [[ -f ${file} ]]; then
         zip  --exclude log/\* -rf ${file} .
@@ -27,4 +42,4 @@ function run(){
 
 }
 
-run
+run $@
